8b626e3425e5c019389246703ddfa5b8b950b6fe
Code Kage <code.kage01@gmail.com>
2026-07-29 00:47:49 +0530

    Create Split Array into Minimum Subsets

---
+++ Split Array into Minimum Subsets.java
@@ -0,0 +1,34 @@
+// Sorting Approach
+
+class Solution {
+    int minSubsets(int arr[]) {
+        Arrays.sort(arr);
+        int count = 1;
+        
+        for(int i=1;i<arr.length;i++){
+            if((arr[i] - arr[i-1]) != 1){
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+
+// Hash Set Approach
+class Solution {
+    int minSubsets(int arr[]) {
+        Set<Integer> s = new HashSet<>();
+        for(int num:arr){
+            s.add(num);
+        }
+        
+        int count = 0;
+        for(int x:arr){
+            if(!s.contains(x-1)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
