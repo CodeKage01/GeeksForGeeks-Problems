457b3f03e850ba7726e1750a8f9b4ce735e0ea61
Code Kage <code.kage01@gmail.com>
2026-08-25 00:49:42 +0530

    Create Minimum Moves to Sort Permutation

---
+++ Minimum Moves to Sort Permutation.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minMoves(int[] arr) {
+        int n = arr.length;
+        
+        int[] pos = new int[n+1];
+        
+        for(int i = 0; i<n;i++){
+            pos[arr[i]] = i;
+        }
+        
+        int maxLen = 1;
+        int curLen = 1;
+        
+        for(int val = 2;val <= n;val++){
+            if(pos[val] > pos[val-1]){
+                curLen++;
+            }else{
+                curLen = 1;
+            }
+            maxLen = Math.max(maxLen, curLen);
+        }
+        
+        return n - maxLen;
+    }
+}
