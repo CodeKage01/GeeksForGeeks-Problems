e05083ac739ce53abe3296afe3521138edc9f29d
Code Kage <code.kage01@gmail.com>
2026-08-29 11:42:14 +0530

    Create Count Subsequences Divisible by n

---
+++ Count Subsequences Divisible by n.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countSubsequences(String s, int n) {
+        long mod = 1_000_000_007;
+        
+        long[] dp = new long[n];
+        for(char c:s.toCharArray()){
+            int curDigit = c-'0';
+            
+            long[] newDp = dp.clone();
+            
+            int rem = curDigit % n;
+            newDp[rem] = (newDp[rem] + 1) % mod;
+            
+            for(int r=0;r<n;r++){
+                int newRem = (r * 10 + curDigit) % n;
+                newDp[newRem] = (newDp[newRem] + dp[r]) % mod;
+            }
+            dp = newDp;
+        }
+        return (int)dp[0];
+    }
+}
+
