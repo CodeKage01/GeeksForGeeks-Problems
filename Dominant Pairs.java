48cb932f734afa83cc10554112fdd708c4620527
Code Kage <code.kage01@gmail.com>
2026-09-16 01:08:32 +0530

    Create Dominant Pairs

---
+++ Dominant Pairs.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int dominantPairs(int[] arr) {
+        int n = arr.length;
+        int half = n / 2;
+        
+        Arrays.sort(arr, 0, half);
+        Arrays.sort(arr, half, n);
+        
+        int j = half;
+        int count = 0;
+        
+        for(int i=0;i<half;i++){
+            while(j < n && 5L*arr[j] <= arr[i]){
+                j++;
+            }
+            count+= j - half;
+        }
+        return count;
+    }
+}
